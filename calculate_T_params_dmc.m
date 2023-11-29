3a636a96a5021123df33421f0d3e068a30092ed5
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:33:30 -0500

    Update calculate_T_params_dmc.m

--- calculate_T_params_dmc.m
+++ calculate_T_params_dmc.m
@@ -1,52 +1,48 @@
-function [T_hat] = calculate_T_params_dmc(Q,eps)
+function [T_hat] = calculate_T_params_dmc(Q, eps)
+%% Calculate parameters for transition matrix T using Hidden Markov Model (HMM)
 
-    N=100000;
-    s_states = {'0','1'};
-    y_states = {'0','1'};
+% Set the number of samples
+N = 100000;
 
-    mu = sum(Q);
-    T=zeros(2,2);
-    for i=1:2
-        for j=1:2
-            T(i,j) = Q(i,j)/mu(1,i);  %% Source state transition matrix
-        end
-    end
+% Define state and symbol sets
+s_states = {'0', '1'};
+y_states = {'0', '1'};
 
-    Emis = 0.5*[(1-eps)+(1-eps) ...
-        (eps)+(eps); ...
-        (eps)+(eps) ...
-        (1-eps)+(1-eps)];
+% Compute mu from the given transition matrix Q
+mu = sum(Q);
 
-    [out_seq, int_s_states] = hmmgenerate(N+1,T,Emis,'Symbols',...
-        y_states,'Statenames',s_states);
+% Build the source state transition matrix T
+T = Q ./ mu;
 
-    b = cell(1,N);
-    for i=1:N
-        temp1 = int_s_states{i};
-        temp2 = int_s_states{i+1};
-        b(1,i) = cellstr(strcat(temp1,temp2));
-    end
+% Define emission matrix Emis for Binary Symmetric Channel (BSC)
+Emis = 0.5 * [(1 - eps) + (1 - eps), (eps) + (eps); (eps) + (eps), (1 - eps) + (1 - eps)];
 
-    V_s_y = hmmdecode(out_seq(2:end),T,Emis,'Symbols',{'0','1'});
+% Generate an output sequence using HMM
+[out_seq, int_s_states] = hmmgenerate(N + 1, T, Emis, 'Symbols', y_states, 'Statenames', s_states);
 
-    T_branch = [T(1,:) 0 0; 0 0 T(2,:);T(1,:) 0 0; 0 0 T(2,:)];
-
-    Emis_branch = [Emis;Emis];
-
-    V_b_y = hmmdecode(out_seq(2:end),T_branch,Emis_branch,...
-        'Symbols',{'0','1'});
-
-    T_hat = zeros(2,2);
-
-    T_hat(1,1) = (sum(log2_entropy(V_b_y(1,:),V_b_y(1,:)))/Q(1,1) ...
-        - sum(log2_entropy(V_s_y(1,:),V_s_y(1,:)))/mu(1,1))/N;
-    T_hat(1,2) = (sum(log2_entropy(V_b_y(2,:),V_b_y(2,:)))/Q(1,2) ...
-        - sum(log2_entropy(V_s_y(1,:),V_s_y(1,:)))/mu(1,1))/N;
-    T_hat(2,1) = (sum(log2_entropy(V_b_y(3,:),V_b_y(3,:)))/Q(2,1) ...
-        - sum(log2_entropy(V_s_y(2,:),V_s_y(2,:)))/mu(1,2))/N;
-    T_hat(2,2) = (sum(log2_entropy(V_b_y(4,:),V_b_y(4,:)))/Q(2,2) ...
-        - sum(log2_entropy(V_s_y(2,:),V_s_y(2,:)))/mu(1,2))/N;
-
-
+% Create state pairs
+b = cell(1, N);
+for i = 1:N
+    b{i} = strcat(int_s_states{i}, int_s_states{i + 1});
 end
 
+% Decode sequences using HMM
+V_s_y = hmmdecode(out_seq(2:end), T, Emis, 'Symbols', {'0', '1'});
+
+% Expand transition matrix and emission matrix for branching HMM
+T_branch = [T(1, :) 0 0; 0 0 T(2, :); T(1, :) 0 0; 0 0 T(2, :)];
+
+Emis_branch = repmat(Emis, 2, 1);
+
+V_b_y = hmmdecode(out_seq(2:end), T_branch, Emis_branch, 'Symbols', {'0', '1'});
+
+% Calculate the estimated transition matrix T_hat
+T_hat = zeros(2, 2);
+for i = 1:2
+    for j = 1:2
+        idx = 2 * (i - 1) + j;
+        T_hat(i, j) = (sum(log2_entropy(V_b_y(idx, :), V_b_y(idx, :))) / Q(i, j) ...
+            - sum(log2_entropy(V_s_y(i, :), V_s_y(i, :))) / mu(i)) / N;
+    end
+end
+end
