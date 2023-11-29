2acf64e7f6b3ff921b523b9508bb549aa8e00d40
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:34:41 -0500

    Update bsc_capacity_rll_source.m

--- bsc_capacity_rll_source.m
+++ bsc_capacity_rll_source.m
@@ -19,16 +19,16 @@ entr_fun = zeros(1, length(eps));
 
 % Iterate over each error probability
 for iter = 1:length(eps)
-    disp(eps(1, iter));
+    disp(eps(iter));
     
     % Initialize capacity variables
     cap = 0;
     prev_cap = 1;
 
-    % Iterate until convergence or cap<0
+    % Iterate until convergence or cap < 0
     while (abs(cap - prev_cap) > delta || cap < 0)
         % Calculate parameters for the transition matrix
-        T = calculate_T_params_dmc(q_r, eps(1, iter));
+        T = calculate_T_params_dmc(q_r, eps(iter));
         T(isnan(T)) = 0;
         A = 2.^(T);
         A(1, 1) = 0;
@@ -39,33 +39,24 @@ for iter = 1:length(eps)
         eig_vec = M(:, index);
 
         % Update transition matrix P
-        P = zeros(2, 2);
-        for i = 1:2
-            for j = 1:2
-                P(i, j) = (eig_vec(j, 1) / eig_vec(i, 1)) * (A(i, j) / m);
-            end
-        end
+        P = eig_vec * (A / m);
 
         % Update mu
         mu = [P(2, 1) / (P(2, 1) + P(1, 2)), P(1, 2) / (P(2, 1) + P(1, 2))];
 
         % Update q_r using P and mu
-        for i = 1:2
-            for j = 1:2
-                q_r(i, j) = P(i, j) * mu(1, i);
-            end
-        end
+        q_r = P .* mu;
 
         % Update capacity
         prev_cap = cap;
         cap = sum(sum(log2_entropy(q_r, 1./P) + q_r.*T));
-        
+
         disp("The capacity at this stage is : " + cap);
-        cap_results(1, iter) = cap;
+        cap_results(iter) = cap;
     end
 
     % Calculate entropy function
-    entr_fun(1, iter) = 1 - log2_entropy(eps(1, iter), 1 / eps(1, iter)) - log2_entropy(1 - eps(1, iter), 1 / (1 - eps(1, iter)));
+    entr_fun(iter) = 1 - log2_entropy(eps(iter), 1 / eps(iter)) - log2_entropy(1 - eps(iter), 1 / (1 - eps(iter)));
 end
 
 % Plot the results
@@ -75,6 +66,4 @@ plot(eps, entr_fun, 'g', 'LineWidth', 1.4);
 legend('Capacity of BSC using RLL source', 'Capacity of BSC in general');
 xlabel('Error prob');
 ylabel('Capacity');
-
-% End of the function
 end
