329944fc4bbf07ad10a4c488b58c2fa4e7a6c2b9
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:34:26 -0500

    Update baa_dmc_cap_sim.m

--- baa_dmc_cap_sim.m
+++ baa_dmc_cap_sim.m
@@ -1,5 +1,5 @@
 function [] = baa_dmc_cap_sim()
-%% Calculates BSC capacity using classical BAA
+%% Calculates Binary Symmetric Channel (BSC) capacity using classical BAA
 
 % Define the range of error probabilities
 eps = 0:0.05:0.5;
@@ -10,17 +10,17 @@ entr_fun = zeros(1, length(eps));
 
 % Iterate over each error probability
 for i = 1:length(eps)
+    disp(eps(i));
     
-    % Transition matrix for the Binary Symmetric Channel (BSC)
-    tran_mat = [1 - eps(1, i), eps(1, i); eps(1, i), 1 - eps(1, i)];
+    % Initialize transition matrix
+    tran_mat = [1 - eps(i), eps(i); eps(i), 1 - eps(i)];
     
     % Calculate channel capacity and input pmf using classical BAA
     [cap, in_pmf] = calculate_cap_dmc(tran_mat);
     
     % Store the calculated channel capacity and entropy function values
-    channel_cap(1, i) = cap;
-    entr_fun(1, i) = 1 - log2_entropy(eps(1, i), 1 / eps(1, i)) - log2_entropy(1 - eps(1, i), 1 / (1 - eps(1, i)));
-    
+    channel_cap(i) = cap;
+    entr_fun(i) = 1 - log2_entropy(eps(i), 1 / eps(i)) - log2_entropy(1 - eps(i), 1 / (1 - eps(i)));
 end
 
 % Plot the results
@@ -28,6 +28,4 @@ plot(eps, channel_cap, 'sr', eps, entr_fun, 'b', 'LineWidth', 1.5);
 legend('Using BAA', '1-h(p)');
 xlabel('Error Probability p');
 ylabel('Capacity');
-
-% End of the function
 end
