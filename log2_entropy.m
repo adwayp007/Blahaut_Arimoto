4a19a76c7d41c423e096c5cf366814ffd7b248a6
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:37:13 -0500

    Update log2_entropy.m

--- log2_entropy.m
+++ log2_entropy.m
@@ -1,10 +1,10 @@
-function [val] = log2_entropy(A,B)
-%% Calculates A.*log_b(B) taking care of 0log 0 scenario
-%% dimensions of A nd B must match
+function val = log2_entropy(A, B)
+%% Calculate A .* log2(B) with handling of 0log0 scenario
+%% Dimensions of A and B must match
 
-val = A.*log2(B);
+% Calculate the element-wise product of A and the base-2 logarithm of B
+val = A .* log2(B);
 
-val(isnan(val))=0;
-
+% Handle cases where log2(0) results in NaN by setting them to 0
+val(isnan(val)) = 0;
 end
-
