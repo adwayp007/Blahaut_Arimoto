9181cadb085060e6c4b5acce580a6c59215c68f3
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:36:45 -0500

    Update calculate_capacity.m

--- calculate_capacity.m
+++ calculate_capacity.m
@@ -1,11 +1,22 @@
 function [I] = calculate_capacity(A)
+%% Calculate channel capacity
 
-[n,m] = size(A);
-I =0;
-for i=1:m
-    temp=log2(A(:,i)./sum(A(:,i)*0.5));
-    temp(isinf(temp))=0;
-    I = I+0.5*sum((A(:,i).*(temp)));
+% Get the size of matrix A
+[n, m] = size(A);
+
+% Initialize the channel capacity
+I = 0;
+
+% Iterate over each column of matrix A
+for i = 1:m
+    % Calculate the conditional probability distribution
+    p_x_given_y = A(:, i) / sum(A(:, i) * 0.5);
+
+    % Handle cases where log2(0) results in -Inf
+    temp = log2(p_x_given_y);
+    temp(isinf(temp)) = 0;
+
+    % Update the channel capacity
+    I = I + 0.5 * sum(A(:, i) .* temp);
 end
 end
-
