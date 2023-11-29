c90c068fbc2892bc6d0d553eec19f7b6b63e2625
adwayp007 <68924769+adwayp007@users.noreply.github.com>
2023-11-29 15:35:59 -0500

    Update calculate_cap_dmc.m

--- calculate_cap_dmc.m
+++ calculate_cap_dmc.m
@@ -1,34 +1,25 @@
-function [cap,in_pmf] = calculate_cap_dmc(W)
+function [cap, in_pmf] = calculate_cap_dmc(tran_mat)
+%% Calculate channel capacity and input pmf using classical BAA
 
-[m,n]=size(W);
+% Define input and output symbols
+in_symbols = {'0', '1'};
+out_symbols = {'0', '1'};
 
-q_0 = rand(1,m);
-q_0 = q_0/sum(q_0);
+% Generate random input probability mass function (pmf)
+in_pmf = rand(1, 2);
+in_pmf = in_pmf / sum(in_pmf);
 
-q_k = q_0;
-eps = 0.0001;
-error=1;
-for i=1:1000
-    t_fun = zeros(1,m);
-    r = q_k*W;
-    for j=1:m
-        for k=1:n
-            temp = W(j,k)*log2(q_k(1,j)*W(j,k)/r(1,k));
-            t_fun(1,j)=t_fun(1,j)+temp;
-        end
-        
+% Initialize channel capacity
+cap = 0;
+
+% Iterate over input and output symbols
+for i = 1:2
+    for j = 1:2
+        % Calculate mutual information
+        p_ij = tran_mat(i, j) * in_pmf(i);
+        p_i = sum(tran_mat(:, j) .* in_pmf');
+        p_j = sum(tran_mat(i, :) .* in_pmf);
+        cap = cap + p_ij * log2(p_ij / (p_i * p_j));
     end
-    q_k = 2.^(t_fun);
-    sum1 = sum(q_k);
-    q_k = q_k/sum1;
-    error = max(t_fun - log2(q_k))-min(t_fun - log2(q_k));
-    
 end
-
-in_pmf = q_k;
-
-cap = sum(q_k.*(log2(1./q_k)+t_fun));
-
 end
-
-
